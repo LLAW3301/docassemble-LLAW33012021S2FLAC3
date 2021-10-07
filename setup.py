b6f76db5a275a9ab0d96d40f90882c1fd2de5aa1
Annarose De Ionno <87924273+deio0004@users.noreply.github.com>
2021-10-07 12:16:59 +1030

    Prototype1

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.LLAW33012021S2FLAC3',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.LLAW33012021S2FLAC3\n\nA docassemble extension.\n\n## Author\n\nAnnarose De Ionno, deio0004@flinders.edu.au\n\n',
+      long_description='# docassemble.LLAW33012021S2FLAC3\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nAnnarose De Ionno, deio0004@flinders.edu.au\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Annarose De Ionno',
       author_email='deio0004@flinders.edu.au',
